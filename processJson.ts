e0a6b0122462ce5c0a63e6d59915976345efd062
Abdelrahman Elbasel <abdelrahman.elbasel42@gmail.com>
2025-03-04 15:56:41 +0200

    Init commit

---
+++ processJson.ts
@@ -0,0 +1,72 @@
+import {
+  quicktype,
+  InputData,
+  jsonInputForTargetLanguage,
+  JSONSchemaInput,
+  FetchingJSONSchemaStore,
+  TargetLanguage,
+} from "quicktype-core";
+
+async function quicktypeJSON(
+  targetLanguage: string | TargetLanguage,
+  typeName: string,
+  jsonString: string
+) {
+  const jsonInput = jsonInputForTargetLanguage(targetLanguage);
+
+  // We could add multiple samples for the same desired
+  // type, or many sources for other types.quicktypeJSON Here we're
+  // just making one type from one piece of sample JSON.
+  await jsonInput.addSource({
+    name: typeName,
+    samples: [jsonString],
+  });
+
+  const inputData = new InputData();
+  inputData.addInput(jsonInput);
+
+  return await quicktype({
+    inputData,
+    lang: targetLanguage,
+  });
+}
+
+async function quicktypeJSONSchema(
+  targetLanguage: string,
+  typeName: string,
+  jsonSchemaString: any
+) {
+  const schemaInput = new JSONSchemaInput(new FetchingJSONSchemaStore());
+
+  // We could add multiple schemas for multiple types,
+  // but here we're just making one type from JSON schema.
+  await schemaInput.addSource({ name: typeName, schema: jsonSchemaString });
+
+  const inputData = new InputData();
+  inputData.addInput(schemaInput);
+
+  return await quicktype({
+    inputData,
+    lang: targetLanguage,
+  });
+}
+
+async function main() {
+  const jsonString = "[]";
+  const jsonSchemaString = "[]";
+  const { lines: swiftPerson } = await quicktypeJSON(
+    "swift",
+    "Person",
+    jsonString
+  );
+  console.log(swiftPerson.join("\n"));
+
+  const { lines: pythonPerson } = await quicktypeJSONSchema(
+    "python",
+    "Person",
+    jsonSchemaString
+  );
+  console.log(pythonPerson.join("\n"));
+}
+
+main();
