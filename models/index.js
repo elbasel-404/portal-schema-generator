41aff6ed0fc7f8a10fe116d2dd1564c06d4168ab
Abdelrahman Elbasel <abdelrahman.elbasel42@gmail.com>
2025-03-06 11:54:33 +0200

    update package

--- models/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.HolidayElementSchema = void 0;
-var schema_1 = require("./holiday/schema");
-Object.defineProperty(exports, "HolidayElementSchema", { enumerable: true, get: function () { return schema_1.HolidayElementSchema; } });
