7afb84a34c1cbdc300895b2caf0128a0c3ca8381
alyadelaly <aliadelhacker123@yahoo.com>
2025-04-06 14:30:51 +0200

    add change bank account endpoint

---
+++ types/change-bank-account/interface.ts
@@ -0,0 +1,238 @@
+// To parse this data:
+//
+//   import { Convert } from "./file";
+//
+//   const changeBankAccount = Convert.toChangeBankAccount(json);
+//
+// These functions will throw an error if the JSON doesn't
+// match the expected interface, even if the JSON is valid.
+
+export interface ChangeBankAccount {
+    id:                         number;
+    name:                       string;
+    current_employee_account:   string;
+    employee_id:                Array<number | string>;
+    new_bank_id:                Array<number | string>;
+    iban:                       string;
+    attachment_ids:             number[];
+    account_status:             boolean | string;
+    state:                      string;
+    order_date:                 Date;
+    refuse_reason:              boolean | string;
+    check_attachment:           boolean;
+    website_message_ids:        any[];
+    message_follower_ids:       number[];
+    message_ids:                number[];
+    message_last_post:          boolean;
+    create_uid:                 Array<number | string>;
+    create_date:                Date;
+    write_uid:                  Array<number | string>;
+    write_date:                 Date;
+    message_is_follower:        boolean;
+    message_partner_ids:        number[];
+    message_channel_ids:        any[];
+    message_unread:             boolean;
+    message_unread_counter:     number;
+    message_needaction:         boolean;
+    message_needaction_counter: number;
+    display_name:               string;
+    __last_update:              Date;
+}
+
+// Converts JSON strings to/from your types
+// and asserts the results of JSON.parse at runtime
+export class Convert {
+    public static toChangeBankAccount(json: string): ChangeBankAccount[] {
+        return cast(JSON.parse(json), a(r("ChangeBankAccount")));
+    }
+
+    public static changeBankAccountToJson(value: ChangeBankAccount[]): string {
+        return JSON.stringify(uncast(value, a(r("ChangeBankAccount"))), null, 2);
+    }
+}
+
+function invalidValue(typ: any, val: any, key: any, parent: any = ''): never {
+    const prettyTyp = prettyTypeName(typ);
+    const parentText = parent ? ` on ${parent}` : '';
+    const keyText = key ? ` for key "${key}"` : '';
+    throw Error(`Invalid value${keyText}${parentText}. Expected ${prettyTyp} but got ${JSON.stringify(val)}`);
+}
+
+function prettyTypeName(typ: any): string {
+    if (Array.isArray(typ)) {
+        if (typ.length === 2 && typ[0] === undefined) {
+            return `an optional ${prettyTypeName(typ[1])}`;
+        } else {
+            return `one of [${typ.map(a => { return prettyTypeName(a); }).join(", ")}]`;
+        }
+    } else if (typeof typ === "object" && typ.literal !== undefined) {
+        return typ.literal;
+    } else {
+        return typeof typ;
+    }
+}
+
+function jsonToJSProps(typ: any): any {
+    if (typ.jsonToJS === undefined) {
+        const map: any = {};
+        typ.props.forEach((p: any) => map[p.json] = { key: p.js, typ: p.typ });
+        typ.jsonToJS = map;
+    }
+    return typ.jsonToJS;
+}
+
+function jsToJSONProps(typ: any): any {
+    if (typ.jsToJSON === undefined) {
+        const map: any = {};
+        typ.props.forEach((p: any) => map[p.js] = { key: p.json, typ: p.typ });
+        typ.jsToJSON = map;
+    }
+    return typ.jsToJSON;
+}
+
+function transform(val: any, typ: any, getProps: any, key: any = '', parent: any = ''): any {
+    function transformPrimitive(typ: string, val: any): any {
+        if (typeof typ === typeof val) return val;
+        return invalidValue(typ, val, key, parent);
+    }
+
+    function transformUnion(typs: any[], val: any): any {
+        // val must validate against one typ in typs
+        const l = typs.length;
+        for (let i = 0; i < l; i++) {
+            const typ = typs[i];
+            try {
+                return transform(val, typ, getProps);
+            } catch (_) {}
+        }
+        return invalidValue(typs, val, key, parent);
+    }
+
+    function transformEnum(cases: string[], val: any): any {
+        if (cases.indexOf(val) !== -1) return val;
+        return invalidValue(cases.map(a => { return l(a); }), val, key, parent);
+    }
+
+    function transformArray(typ: any, val: any): any {
+        // val must be an array with no invalid elements
+        if (!Array.isArray(val)) return invalidValue(l("array"), val, key, parent);
+        return val.map(el => transform(el, typ, getProps));
+    }
+
+    function transformDate(val: any): any {
+        if (val === null) {
+            return null;
+        }
+        const d = new Date(val);
+        if (isNaN(d.valueOf())) {
+            return invalidValue(l("Date"), val, key, parent);
+        }
+        return d;
+    }
+
+    function transformObject(props: { [k: string]: any }, additional: any, val: any): any {
+        if (val === null || typeof val !== "object" || Array.isArray(val)) {
+            return invalidValue(l(ref || "object"), val, key, parent);
+        }
+        const result: any = {};
+        Object.getOwnPropertyNames(props).forEach(key => {
+            const prop = props[key];
+            const v = Object.prototype.hasOwnProperty.call(val, key) ? val[key] : undefined;
+            result[prop.key] = transform(v, prop.typ, getProps, key, ref);
+        });
+        Object.getOwnPropertyNames(val).forEach(key => {
+            if (!Object.prototype.hasOwnProperty.call(props, key)) {
+                result[key] = transform(val[key], additional, getProps, key, ref);
+            }
+        });
+        return result;
+    }
+
+    if (typ === "any") return val;
+    if (typ === null) {
+        if (val === null) return val;
+        return invalidValue(typ, val, key, parent);
+    }
+    if (typ === false) return invalidValue(typ, val, key, parent);
+    let ref: any = undefined;
+    while (typeof typ === "object" && typ.ref !== undefined) {
+        ref = typ.ref;
+        typ = typeMap[typ.ref];
+    }
+    if (Array.isArray(typ)) return transformEnum(typ, val);
+    if (typeof typ === "object") {
+        return typ.hasOwnProperty("unionMembers") ? transformUnion(typ.unionMembers, val)
+            : typ.hasOwnProperty("arrayItems")    ? transformArray(typ.arrayItems, val)
+            : typ.hasOwnProperty("props")         ? transformObject(getProps(typ), typ.additional, val)
+            : invalidValue(typ, val, key, parent);
+    }
+    // Numbers can be parsed by Date but shouldn't be.
+    if (typ === Date && typeof val !== "number") return transformDate(val);
+    return transformPrimitive(typ, val);
+}
+
+function cast<T>(val: any, typ: any): T {
+    return transform(val, typ, jsonToJSProps);
+}
+
+function uncast<T>(val: T, typ: any): any {
+    return transform(val, typ, jsToJSONProps);
+}
+
+function l(typ: any) {
+    return { literal: typ };
+}
+
+function a(typ: any) {
+    return { arrayItems: typ };
+}
+
+function u(...typs: any[]) {
+    return { unionMembers: typs };
+}
+
+function o(props: any[], additional: any) {
+    return { props, additional };
+}
+
+function m(additional: any) {
+    return { props: [], additional };
+}
+
+function r(name: string) {
+    return { ref: name };
+}
+
+const typeMap: any = {
+    "ChangeBankAccount": o([
+        { json: "id", js: "id", typ: 0 },
+        { json: "name", js: "name", typ: "" },
+        { json: "current_employee_account", js: "current_employee_account", typ: "" },
+        { json: "employee_id", js: "employee_id", typ: a(u(0, "")) },
+        { json: "new_bank_id", js: "new_bank_id", typ: a(u(0, "")) },
+        { json: "iban", js: "iban", typ: "" },
+        { json: "attachment_ids", js: "attachment_ids", typ: a(0) },
+        { json: "account_status", js: "account_status", typ: u(true, "") },
+        { json: "state", js: "state", typ: "" },
+        { json: "order_date", js: "order_date", typ: Date },
+        { json: "refuse_reason", js: "refuse_reason", typ: u(true, "") },
+        { json: "check_attachment", js: "check_attachment", typ: true },
+        { json: "website_message_ids", js: "website_message_ids", typ: a("any") },
+        { json: "message_follower_ids", js: "message_follower_ids", typ: a(0) },
+        { json: "message_ids", js: "message_ids", typ: a(0) },
+        { json: "message_last_post", js: "message_last_post", typ: true },
+        { json: "create_uid", js: "create_uid", typ: a(u(0, "")) },
+        { json: "create_date", js: "create_date", typ: Date },
+        { json: "write_uid", js: "write_uid", typ: a(u(0, "")) },
+        { json: "write_date", js: "write_date", typ: Date },
+        { json: "message_is_follower", js: "message_is_follower", typ: true },
+        { json: "message_partner_ids", js: "message_partner_ids", typ: a(0) },
+        { json: "message_channel_ids", js: "message_channel_ids", typ: a("any") },
+        { json: "message_unread", js: "message_unread", typ: true },
+        { json: "message_unread_counter", js: "message_unread_counter", typ: 0 },
+        { json: "message_needaction", js: "message_needaction", typ: true },
+        { json: "message_needaction_counter", js: "message_needaction_counter", typ: 0 },
+        { json: "display_name", js: "display_name", typ: "" },
+        { json: "__last_update", js: "__last_update", typ: Date },
+    ], false),
+};
