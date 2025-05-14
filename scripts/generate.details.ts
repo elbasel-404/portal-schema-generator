d736ba63fde1736fc188c93d1061b655a4008962
elbasel42 <abdelrahman.elbasel42@gmail.com>
2025-05-14 10:27:38 +0300

    fixes

--- scripts/generate.details.ts
+++ scripts/generate.details.ts
@@ -5,7 +5,7 @@ import { writeStringToFile } from "@utils/io";
 import { getModelZodSchema } from "@utils/model";
 
 const SCHEMAS_DIR = "./schemas";
-const TYPES_DIR = "./types";
+// const TYPES_DIR = "./types";
 const JSON_DIR = "./json";
 
 import endpoints from "@lib/endpoints.json";
@@ -22,6 +22,7 @@ endpoints.forEach(async ({ name, url, method, listRequestBody }) => {
     body: JSON.stringify(listRequestBody),
   });
 
+  // ! raw response
   const responseJson = await response.json();
   const responseJsonString = JSON.stringify(responseJson, null, 2);
 
@@ -31,6 +32,7 @@ endpoints.forEach(async ({ name, url, method, listRequestBody }) => {
     data: responseJsonString,
   });
 
+  // ! validate raw response
   const validatedResponseJson = ResponseSchema.parse(responseJson);
   const { id, jsonrpc, result } = validatedResponseJson;
   const {
@@ -39,15 +41,7 @@ endpoints.forEach(async ({ name, url, method, listRequestBody }) => {
     data: responseData,
   } = result;
 
-  // logger.info({
-  //   name,
-  //   id,
-  //   jsonrpc,
-  //   statusCode,
-  //   status,
-  //   data: data.length + " items",
-  // });
-
+  // ! result json
   const dataJsonString = JSON.stringify(responseData, null, 2);
   const dataJsonFilePath = `./${JSON_DIR}/${name}/data.json`;
   await writeStringToFile({
@@ -55,6 +49,7 @@ endpoints.forEach(async ({ name, url, method, listRequestBody }) => {
     data: dataJsonString,
   });
 
+  //  ! zod schema
   const responseDataZodSchema = await getModelZodSchema(name, dataJsonString);
   const responseDataZodSchemaFilePath = `./${SCHEMAS_DIR}/${name}/schema.ts`;
   await writeStringToFile({
@@ -62,26 +57,13 @@ endpoints.forEach(async ({ name, url, method, listRequestBody }) => {
     data: responseDataZodSchema,
   });
 
-  // const responseDataInterface = await getModelInterface({
-  //   modelName: name,
-  //   jsonString: dataJsonString,
-  // });
-  // const responseDataInterfaceFilePath = `./${TYPES_DIR}/${name}/interface.ts`;
-  // await writeStringToFile({
-  //   filePath: responseDataInterfaceFilePath,
-  //   data: responseDataInterface,
-  // });
-
   logger.info({
     data: {
       name,
       status: responseStatus,
       statusCode: responseStatusCode,
-      numItems: responseData.length + " items",
-      responseJsonFilePath,
-      dataJsonFilePath,
-      responseDataZodSchemaFilePath,
-      // responseDataInterfaceFilePath,
+      numItems: responseData.length + " items ",
+      message: "success",
     },
   });
 });
